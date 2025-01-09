853a69fe5b644bae39895c4e8d44680f8c288bbc
Aakash Kc <aakashkcx@gmail.com>
2025-01-09 20:56:15 +0000

    draw planets

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
+import { Body } from "./body";
+import { Canvas } from "./canvas";
+
 import "./style.css";
 
 const canvasElement = document.querySelector<HTMLCanvasElement>("canvas")!;
-const canvasCtx = canvasElement.getContext("2d")!;
 
-canvasCtx.fillStyle = "white";
-canvasCtx.fillRect(100, 100, 100, 100);
+const canvas = new Canvas(canvasElement);
+
+const planets = [
+  new Body("red", 100, 100, 0, 0, 1, 1),
+  new Body("yellow", 100, 100, 0, 0, -1, -1),
+];
+
+function loop() {
+  canvas.clear();
+  for (const planet of planets) {
+    planet.update();
+    planet.draw(canvas);
+  }
+  requestAnimationFrame(loop);
+}
+
+loop();
