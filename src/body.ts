97ee928ec7dc2854936a7d7aa367fc2b7bd8eacb
Aakash Kc <aakashkcx@gmail.com>
2025-01-10 03:09:54 +0000

    add solar system

--- src/body.ts
+++ src/body.ts
@@ -1,4 +1,6 @@
 import { Canvas, Drawable } from "./canvas";
+import { DISTANCE_SCALE, TIME_SCALE } from "./constants";
+import { gravity } from "./physics";
 import { Vector } from "./vector";
 
 export class Body implements Drawable {
@@ -25,13 +27,19 @@ export class Body implements Drawable {
     this.acceleration = acceleration;
   }
 
-  update() {
-    // TODO: calculate gravity
-    this.velocity = this.velocity.add(this.acceleration);
-    this.position = this.position.add(this.velocity);
+  update(bodies: Body[]) {
+    this.acceleration = new Vector();
+    for (const body of bodies)
+      this.acceleration = this.acceleration.add(gravity(this, body));
+    this.velocity = this.velocity.add(this.acceleration.multiply(TIME_SCALE));
+    this.position = this.position.add(this.velocity.multiply(TIME_SCALE));
   }
 
   draw(canvas: Canvas) {
-    canvas.circle(this.position, this.radius, this.color);
+    canvas.circle(
+      this.position.multiply(DISTANCE_SCALE),
+      this.radius,
+      this.color
+    );
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Body } from "./body";
 import { Canvas } from "./canvas";
+import { AU, ME, MS } from "./constants";
 import { Vector } from "./vector";
 
 import "./style.css";
@@ -8,16 +9,49 @@ const canvasElement = document.querySelector<HTMLCanvasElement>("canvas")!;
 
 const canvas = new Canvas(canvasElement);
 
-const planets = [
-  new Body("red", 100, 100, new Vector(0, 0), new Vector(1, 1)),
-  new Body("yellow", 100, 100, new Vector(0, 0), new Vector(-1, -1)),
-];
+const sun = new Body(
+  "yellow", // color
+  25, // radius
+  1 * MS, // mass
+  new Vector(0, 0), // position
+  new Vector(0, 0) // velocity
+);
+const mercury = new Body(
+  "grey", // color
+  10, // radius
+  0.055 * ME, // mass
+  new Vector(-0.387098 * AU, 0), // position
+  new Vector(0, -47.36e3) // velocity
+);
+const venus = new Body(
+  "orange", // color
+  10, // radius
+  0.815 * ME, // mass
+  new Vector(0, -0.723332 * AU), // position
+  new Vector(35.02e3, 0) // velocity
+);
+const earth = new Body(
+  "turquoise", // color
+  10, // radius
+  1 * ME, // mass
+  new Vector(1 * AU, 0), // position
+  new Vector(0, 29.7827e3) // velocity
+);
+const mars = new Body(
+  "red", // color
+  10, // radius
+  0.107 * ME, // mass
+  new Vector(0, 1.52368055 * AU), // position
+  new Vector(-24.07e3, 0) // velocity
+);
+
+const solarSystem = [sun, mercury, venus, earth, mars];
 
 function loop() {
   canvas.clear();
-  for (const planet of planets) {
-    planet.update();
-    planet.draw(canvas);
+  for (const body of solarSystem) {
+    body.update(solarSystem);
+    body.draw(canvas);
   }
   requestAnimationFrame(loop);
 }
