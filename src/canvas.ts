7cadfd1d6ebea25fb8c4a148adb0cac4f5c886e8
Aakash Kc <aakashkcx@gmail.com>
2025-01-10 15:33:40 +0000

    add mouse events

--- src/canvas.ts
+++ src/canvas.ts
@@ -3,24 +3,33 @@ import { Vector } from "./vector";
 export class Canvas {
   element: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
-  zoom: number;
+  scale: number;
+  mouse: {
+    position: Vector;
+    left: boolean;
+    middle: boolean;
+    right: boolean;
+  };
 
   constructor(element: HTMLCanvasElement) {
     this.element = element;
     this.ctx = this.element.getContext("2d")!;
-    this.zoom = 1;
+    this.scale = 1;
+    this.mouse = {
+      position: new Vector(0, 0),
+      left: false,
+      middle: false,
+      right: false,
+    };
 
     this.resize();
     window.addEventListener("resize", this.resize.bind(this));
 
-    this.element.addEventListener("wheel", (event: WheelEvent) => {
-      this.zoom *= event.deltaY < 0 ? 1.25 : 0.8;
-    });
-  }
-
-  resize() {
-    this.element.width = window.innerWidth;
-    this.element.height = window.innerHeight;
+    this.element.addEventListener("mousemove", this.mousemove.bind(this));
+    this.element.addEventListener("mousedown", this.mousedown.bind(this));
+    this.element.addEventListener("mouseup", this.mouseup.bind(this));
+    this.element.addEventListener("wheel", this.wheel.bind(this));
+    this.element.addEventListener("click", this.click.bind(this));
   }
 
   clear() {
@@ -34,10 +43,10 @@ export class Canvas {
   rect2(x: number, y: number, width: number, height: number, color: string) {
     this.ctx.fillStyle = color;
     this.ctx.fillRect(
-      this.zoom * x + this.element.width / 2,
-      this.zoom * y + this.element.height / 2,
-      this.zoom * width,
-      this.zoom * height
+      this.scale * x + this.element.width / 2,
+      this.scale * y + this.element.height / 2,
+      this.scale * width,
+      this.scale * height
     );
   }
 
@@ -49,13 +58,43 @@ export class Canvas {
     this.ctx.fillStyle = color;
     this.ctx.beginPath();
     this.ctx.arc(
-      this.zoom * x + this.element.width / 2,
-      this.zoom * y + this.element.height / 2,
-      this.zoom * radius,
+      this.scale * x + this.element.width / 2,
+      this.scale * y + this.element.height / 2,
+      this.scale * radius,
       0,
       2 * Math.PI
     );
     this.ctx.fill();
+  }
+
+  resize() {
+    this.element.width = window.innerWidth;
+    this.element.height = window.innerHeight;
+  }
+
+  mousemove(event: MouseEvent) {
+    this.mouse.position = new Vector(
+      event.clientX - this.element.width / 2,
+      event.clientY - this.element.height / 2
+    );
+  }
+
+  mousedown(event: MouseEvent) {
+    if (event.button === 0) this.mouse.left = true;
+    else if (event.button === 1) this.mouse.middle = true;
+    else if (event.button === 2) this.mouse.right = true;
+  }
+
+  mouseup(event: MouseEvent) {
+    if (event.button === 0) this.mouse.left = false;
+    else if (event.button === 1) this.mouse.middle = false;
+    else if (event.button === 2) this.mouse.right = false;
+  }
+
+  click(event: MouseEvent) {}
+
+  wheel(event: WheelEvent) {
+    this.scale *= event.deltaY < 0 ? 1.25 : 0.8;
   }
 }
 
