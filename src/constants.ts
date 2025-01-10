62efb480fc4b184dd804074f6b39399c2a5eab0f
Aakash Kc <aakashkcx@gmail.com>
2025-01-10 03:09:13 +0000

    add constants

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+// Gravitational Constant, G
+export const G = 6.6743e-11; // G = 6.6743 * 10^-11
+
+// Astronomical Unit, AU
+export const AU = 1.495978707e11; // 1 AU = 1.495978707 * 10^11 m
+
+// Earth Mass, ME
+export const ME = 5.9722e24; // 1 ME = 5.9722 * 10^24 kg
+
+// Solar Mass, MS
+export const MS = 1.988416e30; // 1 MS = 1.988416 * 10^30 kg
+
+// Distance Scale
+export const DISTANCE_SCALE = 250 / AU; // 250 pixels = 1 AU
+
+// Time Scale
+export const TIME_SCALE = 24 * 60 * 60; // 1 day = 1 frame
